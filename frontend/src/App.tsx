7798dbe0ccb1199b3880466943c69625cd9a233f
Funkicide <104210070+Funkicide@users.noreply.github.com>
2023-03-14 20:23:44 +0300

    Refactor: add responsive layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,24 @@
+import { Container, Row, Col } from "react-bootstrap";
+
 import NotesList from "./components/NotesList";
 import AddNoteForm from "./components/AddNoteForm";
 import EditModal from "./components/EditModal";
 
-import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.scss";
 
 const App = () => {
   return (
-    <main>
-      <AddNoteForm />
-      <NotesList />
-      <EditModal />
-    </main>
+    <Container as="main" fluid="md">
+      <Row className="mt-4" xs={1} md={2}>
+        <Col className="mb-3">
+          <AddNoteForm />
+        </Col>
+        <Col>
+          <NotesList />
+          <EditModal />
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
